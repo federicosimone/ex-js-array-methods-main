a22aab3cf503a8e76f9eef65916b4d04467d1d1f
federicosimone <federico.simone15@gmail.com>
2025-11-20 23:42:07 +0100

    snack 7

--- snack-7/script.js
+++ snack-7/script.js
@@ -21,3 +21,14 @@ const students = [
 
 // Recupera i dati dello studente con id 2
 // Risultato: { id: 2, name: 'Mario Banfi', age: 34, class: '4A' }
+
+//find
+
+const id2Student = students.find(element => {
+  return element.id == 2  ;
+})
+
+console.log(id2Student);
+
+//in questo caso non posso stampare in console element, perchè non esiste fuori dalla funzione
+// quindi devo inserire il tutto all'interno di una variabile.
