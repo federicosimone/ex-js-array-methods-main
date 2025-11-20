94789c960e89459df3db8bf5738e0691c8f38ea3
federicosimone <federico.simone15@gmail.com>
2025-11-20 23:10:25 +0100

    snack 5

--- snack-5/script.js
+++ snack-5/script.js
@@ -2,3 +2,18 @@ const nums = [2, 8, 4, 7, 12, 87];
 
 // Crea un array a partire dall'array nums, che abbia solo i numeri pari
 // Risultato: [2, 8, 4, 12]
+
+//filter()
+
+const numeriPari = nums.filter(element => {
+    if (element % 2 == 0) {
+        return true ;
+    } else {
+        return false ;
+    }
+})
+
+console.log(numeriPari);
+
+//  (element % 2 == 0) è la condizione secondo cui il filter() decide se l'elemento analizzato
+//può entrare o meno nel nuovo array (che deve essere definito da una variabile)
