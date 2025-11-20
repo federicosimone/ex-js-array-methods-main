9eabc8e6979e884e6da924dbbef68c7ce64edd63
federicosimone <federico.simone15@gmail.com>
2025-11-20 22:46:45 +0100

    first commit

---
+++ snack-1/script.js
@@ -0,0 +1,11 @@
+const names = ['Edoardo', 'Simone', 'Francesco'];
+
+// Stampa in console tutti i nomi
+// Risultato: 'Edoardo', 'Simone', 'Francesco'
+ /*names.forEach(function (element) {
+    return console.log(element) ;
+ }) ;*/
+
+
+ names.forEach(element => console.log(element))  ;
+ //il forEach() svolge un'azione su ogni elemento dell'array di origine, senza crearne uno nuovo.
